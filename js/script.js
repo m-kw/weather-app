6053d40da3b5bed59389fa1b4d4f9e2e34b8d951
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-11-08 13:49:54 +0100

    Add toggling between Celsius and Fahrenheit

--- js/script.js
+++ js/script.js
@@ -43,8 +43,23 @@
     const location = document.getElementById('location');
     location.innerHTML = data.name;
 
-    const temp = document.getElementById('temp');
-    temp.innerHTML = data.main.temp + '&deg;';
+    const kTemp = data.main.temp;
+
+    const cTemp = kelvinToCelsius(kTemp);
+
+    const fTemp = kelvinToFahrenheit(kTemp);
+
+    const tempDom = document.getElementById('temp');
+    tempDom.innerHTML = cTemp + '&degC';
+
+    tempDom.addEventListener('click', function() {
+      tempDom.innerHTML = tempDom.innerHTML.replace('\xB0C', '');
+      if (tempDom.innerHTML == cTemp) {
+        tempDom.innerHTML = fTemp + '&degF';
+      } else {
+        tempDom.innerHTML = cTemp + '&degC';
+      }
+    });
 
     const description = document.getElementById('description');
     description.innerHTML = data.weather[0].description;
@@ -52,6 +67,20 @@
     const picture = document.getElementById('weather-pic');
     const iconId = data.weather[0].icon;
     picture.innerHTML = '<img src="http://openweathermap.org/img/wn/' + iconId + '@2x.png">';
+  }
+
+  function kelvinToCelsius(temp) {
+    const celsiusTemp = temp - 273.15;
+    const tempRounded = Math.round(celsiusTemp);
+
+    return tempRounded;
+  }
+
+  function kelvinToFahrenheit(temp) {
+    const fTemp = (temp - 273.15) * 9/5 + 32;
+    const tempRounded = Math.round(fTemp);
+
+    return tempRounded;
   }
 
 }
