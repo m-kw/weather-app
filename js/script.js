808216987e2f44d3a959e1e9e10aee293e784456
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-11-08 15:09:02 +0100

    Get new Weather - add js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 {
+  const select = {
+    apiKey: 'b97ff9652ee99d6757032c1c09248e16',
+  }
+
+  const location = document.getElementById('location');
+  const tempDom = document.getElementById('temp');
+  const description = document.getElementById('description');
+  const picture = document.getElementById('weather-pic');
+
   init();
 
   function init() {
     getCurrentLocation();
+    findLocation();
   }
 
   function getCurrentLocation() {
@@ -23,8 +33,7 @@
   }
 
   function getCurrentWeather(lat, lon) {
-    const key = 'b97ff9652ee99d6757032c1c09248e16';
-    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key;
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + select.apiKey;
     // console.log('url', url);
     // console.log('weather lat', lat);
     // console.log('weather lon', lon);
@@ -40,20 +49,22 @@
   }
 
   function showWeatherData(data) {
-    const location = document.getElementById('location');
     location.innerHTML = data.name;
 
     const kTemp = data.main.temp;
 
     const cTemp = kelvinToCelsius(kTemp);
+    console.log('cTemp', cTemp);
 
     const fTemp = kelvinToFahrenheit(kTemp);
+    console.log('fTemp', fTemp);
 
-    const tempDom = document.getElementById('temp');
     tempDom.innerHTML = cTemp + '&degC';
 
     tempDom.addEventListener('click', function() {
       tempDom.innerHTML = tempDom.innerHTML.replace('\xB0C', '');
+      console.log('tempDom', tempDom.innerHTML);
+
       if (tempDom.innerHTML == cTemp) {
         tempDom.innerHTML = fTemp + '&degF';
       } else {
@@ -61,10 +72,8 @@
       }
     });
 
-    const description = document.getElementById('description');
     description.innerHTML = data.weather[0].description;
 
-    const picture = document.getElementById('weather-pic');
     const iconId = data.weather[0].icon;
     picture.innerHTML = '<img src="http://openweathermap.org/img/wn/' + iconId + '@2x.png">';
   }
@@ -81,6 +90,28 @@
     const tempRounded = Math.round(fTemp);
 
     return tempRounded;
+  }
+
+  function findLocation() {
+    const input = document.querySelector('.newLocation input');
+    const search = document.querySelector('.newLocation .fa-search');
+
+    let newCity;
+
+    search.addEventListener('click', function() {
+      newCity = input.value;
+
+      const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + newCity + '&appid=' + select.apiKey;
+
+      fetch(url)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(newData) {
+          console.log('data', newData);
+          showWeatherData(newData);
+        })
+    });
   }
 
 }
